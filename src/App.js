e65b1842095823a6f5a1d5c9a5311f904cd2e262
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-02-07 16:26:55 +0300

    I have dealt with the X button found at the list to function well

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+  function deleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItems={deleteItems} />
       <Stats />
     </div>
   );
@@ -58,26 +56,24 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItems }) {
   return (
     <div className="list">
       <ul>
-        <span>
-          {items.map((item) => (
-            <Item item={item} />
-          ))}
-        </span>
+        {items.map((item) => (
+          <Item item={item} onDeleteItems={onDeleteItems} key={item.id} />
+        ))}
       </ul>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, onDeleteItems }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
     </li>
   );
 }
