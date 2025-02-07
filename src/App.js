f398f6f68182fba37f4a2b879a485e4f2f93c4fd
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-02-07 17:47:10 +0300

    finished with the footer part

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
         onDeleteItems={deleteItems}
         handleToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -98,10 +98,26 @@ function Item({ item, onDeleteItems, handleToggleItems }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentagePacked =
+    numItems === 0 ? 0 : Math.round((numPacked / numItems) * 100);
+  if (!numItems) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing List💥</em>
+      </p>
+    );
+  }
   return (
     <footer className="stats">
-      👜<em>You have X items on your list, and you already packed X(X%)</em>{" "}
+      👜
+      <em>
+        {percentagePacked === 100
+          ? "You got everything you need! Ready to go✈️"
+          : ` You have ${numItems} items on your list, and you already packed ${numPacked}(${percentagePacked}%)`}
+      </em>
     </footer>
   );
 }
